570148dfd28418207e27f5ee10a331edee1d7e35
Omnisolutions-1 <hopew4727@gmail.com>
2026-06-01 15:06:43 -0500

    Update tone_sensor.py

--- src/pipeline/tone_sensor.py
+++ src/pipeline/tone_sensor.py
@@ -11,16 +11,16 @@ class ToneMatrixSensor:
             "neutral": [0.00, 0.00, 0.00]
         }
         self.shift_constant = 0.05
-        self._max_magnitude = self._compute_max_magnitude()
+        # This now points to our new calculation method below
+        self._max_magnitude = self._compute_true_max_magnitude()
 
-        def _compute_true_max_magnitude(self) -> float:
+    def _compute_true_max_magnitude(self) -> float:
         """Computes the true maximum Euclidean norm of the combined vector space."""
         total_vector = [0.0, 0.0, 0.0]
         for vec in self.mock_matrix.values():
             for i in range(3):
                 total_vector[i] += vec[i] * self.shift_constant
         return math.sqrt(sum(x**2 for x in total_vector))
-
 
     def get_max_magnitude(self) -> float:
         return self._max_magnitude
@@ -34,3 +34,4 @@ class ToneMatrixSensor:
             if token in words:  
                 base_vector = [v + vector[i] * self.shift_constant for i, v in enumerate(base_vector)]
         return base_vector
+
